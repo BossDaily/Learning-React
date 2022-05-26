352d17f2e00c29e85e99a8f76f766d9bb216cd37
BossDaily <comparatorcraft2020@gmail.com>
2022-05-26 13:57:15 -0500

    function inside function

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 const DropdownMenu = () => {
+    const DropdownItem(props) = () => {
+        return(
+            
+        )
+    }
 
     return(
         <div className="dropdown">
 
         </div>
-
     )
 }
 
