094ceda2d2086b6e97c12965d155160d610bd5b2
BossDaily <comparatorcraft2020@gmail.com>
2022-05-26 13:59:23 -0500

    icon right spam

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -5,7 +5,10 @@ const DropdownMenu = () => {
         return(
             <a href="#" className="menu-item">
                 <span className="icon-button">{props.leftIcon}</span>
+
                 {props.children}
+
+                <span className="icon-right">{props.rightIcon}</span>
             </a>
         )
     }
