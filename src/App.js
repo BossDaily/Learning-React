80aaa3e0ef1bf19c0a0adf93d15047e6d39017cc
BossDaily <comparatorcraft2020@gmail.com>
2022-05-26 13:42:33 -0500

    🤓 nav item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "./Nav";
 import './index.css';
+import NavItem from "./NavItem";
 
 const App = () => {
     return (
         <NavBar>
-            
+            <NavItem icon="🤓"/>
         </NavBar>
     )
 }
